a12cfa333b14cd258b6b1ef3e081ad8fd435cc0d
Darío Candotti <dario.candotti@gmail.com>
2022-04-19 21:44:27 -0300

    added zip wrapper tests

--- test/io/ZipPathTest.php
+++ test/io/ZipPathTest.php
@@ -11,12 +11,39 @@ class ZipPathTest
 	/**
 	 * @test
 	 */
-	public function pathShoudlStartWithZipScheme (): void
+	public function pathShouldStartWithZipScheme (): void
 	{
 		$path = strval(new ZipPath(
 			new Path('/usr/home'),
 			new Path('somefile.txt')
 		));
 		$this->assertStringStartsWith('zip://', $path);
+	}
+
+	/**
+	 * @test
+	 */
+	public function pathShouldFollowZipScheme (): void
+	{
+		$zipPath = 'archive.zip';
+		$filePath = 'dir/file.txt';
+
+		$path = new ZipPath(
+			new Path($zipPath),
+			new Path($filePath)
+		);
+		$this->assertEquals("zip://{$zipPath}#{$filePath}", $path->__toString());
+	}
+
+	/**
+	 * @test
+	 */
+	public function toStringMethodShouldBehaveTheSameAsStrvalFunction (): void
+	{
+		$path = new ZipPath(
+			new Path('anything.zip'),
+			new Path('sarasa.txt')
+		);
+		$this->assertEquals(strval($path), $path->__toString());
 	}
 }
